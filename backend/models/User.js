f813873645a6d8cd61072745103f5f096fb58b0d
ruhanaatiq <ruhana.atiq@gmail.com>
2025-12-05 11:28:01 +0600

    updated user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,14 @@
-// backend/models/User.js
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   role: { type: String, default: "student" },
 });
 
-// method to compare password
+// Compare passwords
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return bcrypt.compare(enteredPassword, this.password);
 };
